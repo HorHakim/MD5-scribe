cdbd5d22da8178926a89282b6669773c498b9561
Hakim <horairyhakim@gmail.com>
2026-07-06 15:29:51 +0200

    refactor: the speech to text agent is now following the POO patern

--- src/speech_to_text_agent.py
+++ src/speech_to_text_agent.py
@@ -5,23 +5,34 @@ from groq import Groq
 
 from config import GROQ_API_KEY, STT_MODEL
 
-client = Groq(api_key=GROQ_API_KEY)
 
 
+class SpeechToTextAgent:
+	def __init__(self):
+		self.client = Groq(api_key=GROQ_API_KEY)
 
-def get_text_from_audio(file_path):
-	with open(file_path, "rb") as file:
 
-		transcription = client.audio.transcriptions.create(
-			file=file,
-			model=STT_MODEL,
-			response_format="json",
-			temperature=0.0
-		)
+	def get_text_from_audio(self, file_path):
+		
+		with open(file_path, "rb") as file:
 
-		return transcription.text
+			transcription = self.client.audio.transcriptions.create(
+				file=file,
+				model=STT_MODEL,
+				response_format="json",
+				temperature=0.0
+			)
+
+			return transcription.text
+
+
 
 if __name__ == "__main__":
-	file_path = "./data/AUDIO.mp3" # Replace with your audio file!
-	extracted_text = get_text_from_audio(file_path)
+	
+	speech_to_text_agent = SpeechToTextAgent()
+
+
+	file_path = "./data/AUDIO.mp3" # Replace with your audio file
+	extracted_text = speech_to_text_agent.get_text_from_audio(file_path)
+
 	print(extracted_text)
