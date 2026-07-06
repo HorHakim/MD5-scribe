d6c591f5048be271cac15338dd8854a8e7df0b7a
Hakim <horairyhakim@gmail.com>
2026-07-06 16:35:38 +0200

    feature: now we have an agent manager that takes an audio as input and produces an information extraction in a json format

--- src/speech_to_text_agent.py
+++ src/speech_to_text_agent.py
@@ -7,9 +7,9 @@ class SpeechToTextAgent(Agent):
 		super().__init__()
 
 
-	def get_text_from_audio(self, file_path):
+	def get_text_from_audio(self, audio_file_path):
 		
-		with open(file_path, "rb") as file:
+		with open(audio_file_path, "rb") as file:
 
 			transcription = self.client.audio.transcriptions.create(
 				file=file,
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 	speech_to_text_agent = SpeechToTextAgent()
 
 
-	file_path = "./data/AUDIO.mp3" # Replace with your audio file
-	extracted_text = speech_to_text_agent.get_text_from_audio(file_path)
+	audio_file_path = "./data/AUDIO.mp3"
+	transcription_text = speech_to_text_agent.get_text_from_audio(audio_file_path)
 
-	print(extracted_text)
+	print(transcription_text)
